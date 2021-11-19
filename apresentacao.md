29934e520b31300301819b405c18ef24442b3756
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2021-11-19 19:03:05 -0300

    interface Netflix

---
+++ apresentacao.md
@@ -0,0 +1,17 @@
+# Recriação da interface da Netflix :film_projector: 
+
+## Adicionei algumas funcionalidades diferentes da Netflix, mas usei a sua interface como base.
+
+<br/>
+
+## Atividade proposta pela Digital Innovation One.
+
+
+ - Usei como referência o seguinte projeto: https://github.com/felipeAguiarCode/netflix-clone
+
+
+<br/>
+
+### **OBS:**
+  Atalho no HTML:
+Alt + shift + seta pra baixo = faz uma cópia da linha pra baixo daquela linha
---
+++ js/owl/setup.js
@@ -0,0 +1,26 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+function linkTrailerTWD(){
+    window.location.href="https://www.youtube.com/watch?v=sfAc2U20uyg&t=10s";
+    //<a href="https://www.youtube.com/watch?v=sfAc2U20uyg&t=10s"></a>
+}
+
+function linkInfosTWD(){
+    window.location.href="/atoresSeriePrincipal.html";
+    //<a href="https://www.youtube.com/watch?v=sfAc2U20uyg&t=10s"></a>
+}
